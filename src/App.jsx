d8d0e5f3204e54c7f246e5c8135db9ca9276e2df
krishna-87932 <matrix202440@gmail.com>
2026-07-16 17:45:20 +0530

    Fixed The Dark and Light Mode

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,27 @@ import './App.css'
 import Navbar from "./components/Navbar"
 import TextForm from './components/TextForm'
 // import About from './components/About'
-function App() {
+import { useState } from 'react'
 
+// 212529
+
+function App() {
+  const [mode, setMode] = useState('light')
+  const toggleMode=()=>{
+    if(mode === 'dark'){
+      setMode('light')
+      document.body.style.backgroundColor = "#e8ebee"
+    }
+    else{
+      setMode('dark')
+      document.body.style.backgroundColor = "#232528"
+    }
+  }
   return (
     <>
 
-      <Navbar title="TextUtils" />
-      <TextForm heading='Put Text Here.' />
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <TextForm heading='Put Text Here.'mode={mode} />
       {/* <About/> */}
 
     </>
--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -2,6 +2,13 @@ import {useState} from 'react'
 
 
 function TextForm(props) {
+    const textColor = props.mode === 'dark' ? 'white' : 'black';
+    const textAreaStyle = {
+        backgroundColor: props.mode === 'dark' ? '#343a40' : 'white',
+        color: textColor,
+        borderColor: props.mode === 'dark' ? '#6c757d' : '#ced4da'
+    };
+
     const handelUpclick =()=>{
         let newText = text.toUpperCase();
         setText(newText)
@@ -26,15 +33,15 @@ function TextForm(props) {
     const [text, setText] = useState("")
     return (
         <>
-            <div className="container mb-10 mt-5">
+            <div className="container mb-10 mt-5" style={{color : textColor}}>
             <h3>{props.heading}</h3>
-                <textarea className="form-control" onChange={handelOnChange} placeholder='Put You Text Here..' value={text} id="myBox" rows="5"></textarea>
+                <textarea className="form-control" onChange={handelOnChange} placeholder='Put You Text Here..' value={text} id="myBox" rows="5" style={textAreaStyle}></textarea>
                 <button type="button" className="btn btn-danger mt-2 mx-2" onClick={clearText}>Clear Text</button>
                 <button type="button" className="btn btn-primary mt-2 mx-2" onClick={handelUpclick}>To Upper Case</button>
                 <button type="button" className="btn btn-primary mt-2 mx-2" onClick={handelLowclick}>To Lower Case</button>
                 <button type="button" className="btn btn-primary mt-2 mx-2" onClick={handelCapclick}>Capitalised Text</button>
             </div>
-            <div className="container">
+            <div className="container " style={{color : textColor}}>
                 <hr />
                 <h2>Text Summary here</h2>
                 <p> <b>Words:</b>  {text.split(" ").length} and <b>Characters:</b> {text.length}</p> 
